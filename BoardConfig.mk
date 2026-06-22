ahmedtohamy1/android_device_xiaomi_peridot

peridot: Bring back mispatializer, misoundfx and dolby stack
This reverts commit d2ea4d399e6c6d7f600a32cb52f0c2484cd49460.

* Bring dax configs to dt

Change-Id: I9f8eaf4a2caba3af472110ffd769fcd461478761

BoardConfig.mk
@@ -68,6 +68,7 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/device_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 

halcyonlv/device_xiaomi_peridot

BoardConfig.mk
@@ -68,6 +68,7 @@ BOARD_USES_QCOM_HARDWARE := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/hidl/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 